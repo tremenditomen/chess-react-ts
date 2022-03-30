d1beb1e18bca09497991c52a22ffa42e4fbc8cb2
Luis Andres Contreras <tremenditomen@gmail.com>
2022-03-29 19:07:48 -0600

    had to switch to tsx :D

---
+++ src/componentes/Chessboard/Chessboard.tsx
@@ -0,0 +1,18 @@
+import "./Chessboard.css";
+import Tile from '../Tile/Tile';
+
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+export default function Chessboard() {
+  let board = [];
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      const number = j + i + 2;
+      board.push(<Tile number = {number}/> )
+      
+    }
+  }
+
+  return <div id="chessboard">{board}</div>;
+}
